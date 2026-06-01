4450aa45d90249a0c0c0c217e0387fd73e1ec65f
Pareekshith Raj <pareekshith913@gmail.com>
2026-06-01 22:12:05 +0530

    fix: prevent employee login redirect loop

--- src/app/employee/login/page.tsx
+++ src/app/employee/login/page.tsx
@@ -1,9 +1,5 @@
-import { redirect } from "next/navigation";
-import { getEmployeeSession } from "@/lib/employee/session";
 import LoginClient from "./LoginClient";
 
 export default async function EmployeeLoginPage() {
-  const session = await getEmployeeSession();
-  if (session) redirect("/employee/portal");
   return <LoginClient />;
 }
